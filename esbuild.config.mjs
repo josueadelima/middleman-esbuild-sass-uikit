578540c094ea5a62ad1a9845ec32da6cfbb126f3
Josué A. de Lima <josueadelima@gmail.com>
2024-08-01 23:55:03 -0600

    Minor esbuild code cleanup

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -8,17 +8,10 @@ const context = await esbuild.context({
   minify: process.argv.includes("--minify"),
   sourcemap: process.argv.includes("--sourcemap"),
 })
+
 if(process.argv.includes("--watch")) {
-  // Manually do an incremental build
-  await context.rebuild()
-
-  // Enable watch mode
   await context.watch()
-
-  // Enable serve mode
-  await context.serve()
 } else {
   await context.rebuild()
-
   context.dispose()
 }
